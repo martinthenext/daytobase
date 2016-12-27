13c9f531df1ac95db92b9304c03996f0b914dc2b
martinthenext <martin.davtyan@gmail.com>
2016-12-27 02:57:09 +0000

    Settings file stub fix

--- settings.py
+++ settings.py
@@ -1,11 +1,11 @@
 # Your Telegram bot access key
 BOT_ACCESS_KEY = '<YOUR_BOT_KEY>'
 
-# Temporary dir for exports. Ideally you will clean up once in a while
-TEMP_DIR = '~/daytobase_temp'
+# Temporary dir for exports. Ideally you would clean up once in a while
+TEMP_DIR = '/tmp'
 
 # External web server setup
 # A directory on your server that serves static content
-STATIC_DIR = '/var/www/'
+STATIC_DIR = '/var/www/daytobase'
 # A URL at which this static content is served
 STATIC_URL = r'http://localhost/daytobase'
