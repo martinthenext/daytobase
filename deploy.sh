3d524876ba5136ec87667fb820bdc5402d131e06
2mol <2mol@users.noreply.github.com>
2020-07-27 18:53:04 +0200

    api gateway

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,7 @@ terraform apply -auto-approve
 # test
 aws lambda invoke --cli-binary-format raw-in-base64-out --log-type Tail --function-name minimal_lambda_function --payload '{ "name": "Bob" }' response.json | jq .LogResult | sed 's/"//g' | base64 --decode
 
+curl -X POST -H "Content-Type: application/json" -d '{ "name": "Bob" }' "$(terraform output api_url)"
+
 # cleanup
 rm lambda.zip
-
--- main.tf
+++ main.tf
@@ -15,7 +15,7 @@ variable "runtime" {
   default = "python3.8"
 }
 
-resource "aws_lambda_function" "lambda_function" {
+resource "aws_lambda_function" "daytobase" {
   role             = aws_iam_role.lambda_exec_role.arn
   handler          = var.handler
   runtime          = var.runtime
@@ -43,4 +43,45 @@ resource "aws_iam_role" "lambda_exec_role" {
   ]
 }
 EOF
+}
+
+# -----------------------------------------------------------------------------
+# fucking API GATEWAY
+# -----------------------------------------------------------------------------
+
+resource "aws_api_gateway_rest_api" "lambda_api" {
+  name = "lambda_api"
+}
+
+resource "aws_api_gateway_resource" "resource" {
+  path_part   = "api"
+  parent_id   = aws_api_gateway_rest_api.lambda_api.root_resource_id
+  rest_api_id = aws_api_gateway_rest_api.lambda_api.id
+}
+
+resource "aws_api_gateway_method" "method" {
+  rest_api_id   = aws_api_gateway_rest_api.lambda_api.id
+  resource_id   = aws_api_gateway_resource.resource.id
+  http_method   = "POST"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "integration" {
+  rest_api_id             = aws_api_gateway_rest_api.lambda_api.id
+  resource_id             = aws_api_gateway_resource.resource.id
+  http_method             = aws_api_gateway_method.method.http_method
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = "arn:aws:apigateway:${var.region}:lambda:path/2015-03-31/functions/${aws_lambda_function.daytobase.arn}/invocations"
+}
+
+resource "aws_api_gateway_deployment" "deployment" {
+  depends_on = [aws_api_gateway_integration.integration]
+
+  rest_api_id = aws_api_gateway_rest_api.lambda_api.id
+  stage_name  = "prod"
+}
+
+output "api_url" {
+  value = "${aws_api_gateway_deployment.deployment.invoke_url}${aws_api_gateway_resource.resource.path}"
 }
