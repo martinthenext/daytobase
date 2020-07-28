cf9392c62b698059b73d9a05338c1cc93e1ab5ae
2mol <2mol@users.noreply.github.com>
2020-07-28 17:03:31 +0200

    minimise endpoints

--- main.tf
+++ main.tf
@@ -90,23 +90,23 @@ resource "aws_api_gateway_rest_api" "lambda_api" {
   description = "Daytobase serverless server that serves requests."
 }
 
-resource "aws_api_gateway_resource" "proxy" {
+resource "aws_api_gateway_resource" "api" {
   rest_api_id = aws_api_gateway_rest_api.lambda_api.id
   parent_id   = aws_api_gateway_rest_api.lambda_api.root_resource_id
-  path_part   = "{proxy+}"
+  path_part   = "api"
 }
 
-resource "aws_api_gateway_method" "proxy" {
+resource "aws_api_gateway_method" "api" {
   rest_api_id   = aws_api_gateway_rest_api.lambda_api.id
-  resource_id   = aws_api_gateway_resource.proxy.id
+  resource_id   = aws_api_gateway_resource.api.id
   http_method   = "ANY"
   authorization = "NONE"
 }
 
 resource "aws_api_gateway_integration" "lambda" {
   rest_api_id = aws_api_gateway_rest_api.lambda_api.id
-  resource_id = aws_api_gateway_resource.proxy.id
-  http_method = aws_api_gateway_method.proxy.http_method
+  resource_id = aws_api_gateway_resource.api.id
+  http_method = aws_api_gateway_method.api.http_method
 
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
@@ -115,29 +115,29 @@ resource "aws_api_gateway_integration" "lambda" {
 
 # ----- API root bullshit
 
-resource "aws_api_gateway_method" "proxy_root" {
-  rest_api_id   = aws_api_gateway_rest_api.lambda_api.id
-  resource_id   = aws_api_gateway_rest_api.lambda_api.root_resource_id
-  http_method   = "ANY"
-  authorization = "NONE"
-}
+# resource "aws_api_gateway_method" "proxy_root" {
+#   rest_api_id   = aws_api_gateway_rest_api.lambda_api.id
+#   resource_id   = aws_api_gateway_rest_api.lambda_api.root_resource_id
+#   http_method   = "ANY"
+#   authorization = "NONE"
+# }
 
-resource "aws_api_gateway_integration" "lambda_root" {
-  rest_api_id = aws_api_gateway_rest_api.lambda_api.id
-  resource_id = aws_api_gateway_method.proxy_root.resource_id
-  http_method = aws_api_gateway_method.proxy_root.http_method
+# resource "aws_api_gateway_integration" "lambda_root" {
+#   rest_api_id = aws_api_gateway_rest_api.lambda_api.id
+#   resource_id = aws_api_gateway_method.proxy_root.resource_id
+#   http_method = aws_api_gateway_method.proxy_root.http_method
 
-  integration_http_method = "POST"
-  type                    = "AWS_PROXY"
-  uri                     = aws_lambda_function.daytobase.invoke_arn
-}
+#   integration_http_method = "POST"
+#   type                    = "AWS_PROXY"
+#   uri                     = aws_lambda_function.daytobase.invoke_arn
+# }
 
 #  ----
 
 resource "aws_api_gateway_deployment" "deployment" {
   depends_on = [
     aws_api_gateway_integration.lambda,
-    aws_api_gateway_integration.lambda_root,
+    # aws_api_gateway_integration.lambda_root,
   ]
   rest_api_id = aws_api_gateway_rest_api.lambda_api.id
   stage_name  = "prod"
