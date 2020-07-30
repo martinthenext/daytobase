ac955d97e4ca3b98c428e68923cc76201c21ea59
2mol <2mol@users.noreply.github.com>
2020-07-30 10:39:40 +0200

    maybe fix gateway deployment problem, need to test

--- main.tf
+++ main.tf
@@ -141,6 +141,10 @@ resource "aws_api_gateway_deployment" "deployment" {
   ]
   rest_api_id = aws_api_gateway_rest_api.lambda_api.id
   stage_name  = "prod"
+
+  lifecycle {
+    create_before_destroy = true
+  }
 }
 
 output "api_url" {
