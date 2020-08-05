52d17eca31e66c13ba8ed3aece56205b6333c586
Martin Davtyan <Davtyan.Martin@bcg.com>
2020-08-05 13:17:14 +0100

    Echo bot implemented and hooked to the telegram webhook

--- lambda/main.py
+++ lambda/main.py
@@ -1,12 +1,24 @@
 import os
 import json
+from urllib import request, parse
 
 def handler(event, context):
-    return_body = {
-        'env': os.environ.__repr__(),
-        'TELEGRAM_TOKEN': os.environ.get('TELEGRAM_TOKEN')
-    }
+    body = json.loads(event["body"])
+    print("Body")
+    print(body)
+
+    chat_id = body["message"]["chat"]["id"]
+    text = body["message"]["text"]
+
+    token = os.environ.get('TELEGRAM_TOKEN')
+    send_url = f"https://api.telegram.org/bot{token}/sendMessage"
+
+    data = {"chat_id": chat_id, "text": f"You said {text}"}
+    data_encoded = parse.urlencode(data).encode()
+    req =  request.Request(send_url, data=data_encoded)
+    resp = request.urlopen(req)
+
     return {
         'statusCode': 200,
-        'body': json.dumps(return_body)
+        'body': json.dumps({}),
     }
